a7452f140c4706d95a985db07b91c2ea7c214fa0
hepzishanthijohn <yyepsipal@gmail.com>
2024-04-16 13:50:03 +0530

    MernStack

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require('./db');
 const adminRoutes = require ('./routes/adminRoutes');
 const studentRoutes = require ('./routes/studentRoutes');
-
+const mentorRoutes = require('./routes/mentorRoutes')
 
 
 
@@ -16,7 +16,7 @@ const cors = require('cors');
 app.use(cors());
 app.use('/admin',adminRoutes)
 app.use('/student',studentRoutes);
-
+app.use('/mentor',mentorRoutes);
 
 
 const PORT = process.env.PORT || 5000;
