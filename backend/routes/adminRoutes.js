a9b463ca818a1d46b6276c162a3c0441cfd89d7e
hepzishanthijohn <yyepsipal@gmail.com>
2024-04-16 20:21:10 +0530

    MernStack

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -13,39 +13,27 @@ const router = express.Router();
 
 // POST /auth/register
 router.post("/AdminReg", async (req, res) => {
-  const { name, email, password,institutionName } = req.body;
-
   try {
-    let Admin = await admin.findOne({ email });
-    if (Admin) {
-      return res.status(400).json({ msg: "Admin already exists" });
-    }
+      const admin = new Admin({
+          ...req.body
+      });
 
-    Admin = new admin({
-      name,
-      email,
-      password,
-      institutionName
-    });
+      const existingAdminByEmail = await Admin.findOne({ email: req.body.email });
+      const existingSchool = await Admin.findOne({ schoolName: req.body.schoolName });
 
-    await Admin.save();
-    // const payload = {
-    //     user: {
-    //       id: user._id,
-    //     },
-    //   };
-    res.status(201).json({ msg: "Admin registered successfully" });
-
-    
-
-    // jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" }),
-    //   (err, token) => {
-    //     if (err) throw err;
-    //     res.json({ token });
-    //   };
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server error");
+      if (existingAdminByEmail) {
+          res.send({ message: 'Email already exists' });
+      }
+      else if (existingSchool) {
+          res.send({ message: 'School name already exists' });
+      }
+      else {
+          let result = await admin.save();
+          result.password = undefined;
+          res.send(result);
+      }
+  } catch (err) {
+      res.status(500).json(err);
   }
 });
 
@@ -76,47 +64,49 @@ router.get('/', async (req, res) => {
 });
 
 // POST /auth/login
-router.post("/AdminLogin", async (req, res) => {
-  const {email, password} = req.body;
-  const name =req.body;
-
-  try {
-    let Admin = await admin.findOne({ email });
-    if (!Admin) {
-      return res.status(400).json({ msg: "Invalid Credentials" });
-    }else{
-      console.log(Admin.name)
+router.post("/AdminLogin",async (req, res) => {
+ try {
+  if (req.body.email && req.body.password) {
+    let admin = await Admin.findOne({ email: req.body.email });
+    if (admin) {
+        const passwordMatch = await bcrypt.compare(req.body.password, admin.password);
+        if (passwordMatch) {
+            admin.password = undefined;
+            res.send(admin);
+        } else {
+            res.send({ message: "Invalid password" });
+        }
+    } else {
+        res.send({ message: "User not found" });
     }
+} else {
+    res.send({ message: "Email and password are required" });
+}
+const payload = {
+  Admin: {
+    name: Admin.name,
+    id: Admin._id,
+    institution_Name:Admin.institutionName,
+    role: Admin.role,
+  },
+};
 
-    const isMatch = await bcrypt.compare(password, Admin.password,console.log(Admin.name));
-    
-    if (!isMatch) {
-      return res.status(400).json({ msg: "Invalid Credentials" });
-    }
-
-    const payload = {
-      Admin: {
-        name: Admin.name,
-        id: Admin._id,
-        institution_Name:Admin.institutionName,
-        role: Admin.role,
-      },
-    };
-
-    jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      }
-    );
-  } catch (error) {
+jwt.sign(
+  payload,
+  process.env.JWT_SECRET,
+  { expiresIn: "1h" },
+  (err, token) => {
+    if (err) throw err;
+    res.json({ token });
+  }
+)
+  
+}catch (error) {
     console.error(error.message);
     res.status(500).send("Server error");
   }
-});
+})
+
 
 // GET /auth/logout
 router.get("/logout", (req, res) => {
